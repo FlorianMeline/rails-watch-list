3355d29ab007835b23259fb18c067bbb27766f92
Florian Meline <florian_meline@live.fr>
2023-05-25 16:43:34 +0200

    commit de fin d'am

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "lists#index"
+  resources :lists, except: :index do
+    resources :bookmarks, only: [:create]
+  end
+
 end
